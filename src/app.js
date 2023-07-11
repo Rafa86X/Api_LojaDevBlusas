73225a27b5b2b49e52d408bd124e84838803511b
Rafael Junges <rafadevweb@gmail.com>
2023-07-10 21:19:17 -0300

    Adicionando cors para evitar cors policy

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import db from "./config/dbConnect.js";
 import routes from "./routes/index.js";
 
 
+
 db.on("error", console.log.bind(console, 'Erro de conexao'))
 db.once("open", () => {
     console.log('conexao ok')
@@ -11,5 +12,6 @@ db.once("open", () => {
 const app = express()
 app.use(express.json())
 routes(app);
+
 
 export default app
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import produtos from "./produtoRoutes.js"
+import cors from "cors"
 
 const routes = (app) => {
+app.use(cors())
+
   app.route('/').get((req, res) => {
     res.status(200).send({titulo: "Loja DevBlusas"})
   })
